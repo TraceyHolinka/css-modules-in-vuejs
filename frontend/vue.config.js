fa3940db1e8f2bf510e830bac7635b9b15054fa2
Tracey Holinka <tracey.holinka@gmail.com>
2020-04-13 22:38:39 -0400

    update frontend dependancies

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   productionSourceMap: false,
   css: {
-    extract: false,
-    loaderOptions: {
-      css: {
-        use: ['style-loader', 'postcss-loader'], // default setting
-        localIdentName: '[name]_[local]_[hash:base64:5]'
-      }
-    }
+    extract: false
   },
   lintOnSave: true
 }
